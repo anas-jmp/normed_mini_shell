1cdd413762cfc192760d2899aecc8746555115bb
nxa0 <155154070+anas-jmp@users.noreply.github.com>
2024-12-22 19:00:33 +0100

    Update launch_processes.c

--- src/execution/launch_processes.c
+++ src/execution/launch_processes.c
@@ -1,81 +1,91 @@
 #include "../../include/execution.h"
  
+int is_builtin_command(const char *command)
+{
+    return (!strcmp(command, "export") ||  !strcmp(command, "unset") || !strcmp(command, "exit"));
+}
+
 
 
 void	execute_first_child_process(t_command *prompt, char **env, int **pipefd,
 		int i)
 {
 	t_shell_state shell_state;
-      shell_state.state = 0; 
+      int x;
+	  shell_state.state = 0; 
       shell_state.exit_status = 0;
 
-        if (!strcmp(prompt->command, "export") || !strcmp(prompt->command, "unset") || !strcmp(prompt->command, "exit")  ) 
-        {
-             return;
-        }
-
+		 x=0;
+       if (!strcmp(prompt->command, "export") || !strcmp(prompt->command, "unset") ||  !strcmp(prompt->command, "exit") ) 
+				x=1;
 if (!strcmp(prompt->command, "exit"))
   {
      handle_exit(prompt->args, &shell_state);  
   }
-	if (prompt->command && strcmp(prompt->command,"export"))
+// if (prompt->command)
+if (prompt->command)
 	{
 		prompt->cmd_path = is_command(prompt->command, env);
-		first_child_processe(prompt, env, pipefd, i);
+	   if(x!=1)
+		 {
+			first_child_processe(prompt, env, pipefd, i);
+		}
 	}
  
 }
 
-void	execute_middle_child_process(t_command *prompt, char **env,
-		int **pipefd, int i)
+void	execute_middle_child_process(t_command *prompt, char **env,	int **pipefd, int i)
 {
 	    t_shell_state shell_state;
       shell_state.state = 0; 
       shell_state.exit_status = 0;
+	  int x=0;
 
-        if (!strcmp(prompt->command, "export") || !strcmp(prompt->command, "unset") || !strcmp(prompt->command, "exit")  ) 
+       if (!strcmp(prompt->command, "export") || !strcmp(prompt->command, "unset") ||  !strcmp(prompt->command, "exit") ) 
         {
-             return;
-        }
-
-  if (!strcmp(prompt->command, "exit")) {
-     handle_exit(prompt->args, &shell_state);  
-  }
-	if (prompt->command && strcmp(prompt->command,"export" ) )
-	{
+				x=1;
+         }
+  if (!strcmp(prompt->command, "exit"))
+    {
+      handle_exit(prompt->args, &shell_state);  
+    }
+ if (prompt->command)
+{
 		prompt->cmd_path = is_command(prompt->command, env);
 		if (pipe(pipefd[i]) < 0)
 			output_error_exit("pipe", EXIT_FAILURE);
-		middle_child_processes(prompt, pipefd, env, i);
-	}
+		if(x!=1)
+			{
+					middle_child_processes(prompt, pipefd, env, i);
+			}
+}
 }
 void	execute_ending_child_process(t_command *prompt, char **env,
 		int **pipefd, int i)
 {
+	
 	  t_shell_state shell_state;
-      shell_state.state = 0; 
+ 	  int x;
+      
+	  shell_state.state = 0; 
       shell_state.exit_status = 0;
-
-           if (!strcmp(prompt->command, "export") || !strcmp(prompt->command, "unset") || !strcmp(prompt->command, "exit")  ) 
-        {
-             return;
-        }
-
+	  x=0;
+       if (!strcmp(prompt->command, "export") || !strcmp(prompt->command, "unset") ||  !strcmp(prompt->command, "exit") ) 
+				x=1;
   if (!strcmp(prompt->command, "exit")) {
      handle_exit(prompt->args, &shell_state);  
   }
-	if (prompt->command && strcmp(prompt->command,"export"))
+  if (prompt->command )
 	{
 		prompt->cmd_path = is_command(prompt->command, env);
 		if (pipe(pipefd[i]) < 0)
 			output_error_exit("pipe", EXIT_FAILURE);
-		ending_child_processe(prompt, pipefd, env, i);
+		if(x!=1)
+		{
+			ending_child_processe(prompt, pipefd, env, i);
+		}
 	}
 }
-
-
-
-
 
 
 void	exec_one_process(t_command *prompt, char **env)
@@ -84,8 +94,8 @@ void	exec_one_process(t_command *prompt, char **env)
 	char	*cmd_path;
 	int		fd_in_out[2];
 
-        if (!strcmp(prompt->command, "export") || !strcmp(prompt->command, "unset") || !strcmp(prompt->command, "exit")  ) 
-             return;
+        if (!strcmp(prompt->command, "export") || !strcmp(prompt->command, "unset") )
+				return;
 	fd_in_out[0] = STDIN_FILENO;
 	fd_in_out[1] = STDOUT_FILENO;
 	if (!prompt->command)
@@ -108,4 +118,5 @@ void	exec_one_process(t_command *prompt, char **env)
 	}
 	wait(NULL);
 }
+
 
